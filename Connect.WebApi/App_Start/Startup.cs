b48b210ea62ab70622fac5a64e3eb7e9f202df09
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2013-11-03 09:41:08 +0100

    requried packages

---
+++ Connect.WebApi/App_Start/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace Connect.WebApi
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "webapi/{controller}/{id}",
+                defaults: new
+                    {
+                        controller= "Values",
+                        id = RouteParameter.Optional
+                    }
+            );
+
+            appBuilder.UseWebApi(config);
+        }
+    } 
+}
